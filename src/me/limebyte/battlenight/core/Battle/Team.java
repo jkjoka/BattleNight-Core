e81b631c708fac4771fed71429f620b599c08487
LimeByte <limebyte.endercraft@gmail.com>
2012-04-15 22:20:38 +1200

    Basic start.

--- src/me/limebyte/battlenight/core/Battle/Team.java
+++ src/me/limebyte/battlenight/core/Battle/Team.java
@@ -1,10 +1,21 @@
 package me.limebyte.battlenight.core.Battle;
 
+import org.bukkit.ChatColor;
+
 /**
  * @author LimeByte.
  * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-nd/3.0/
  */
 public class Team {
-
+	private String name;
+	private ChatColor chatColor;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ChatColor getChatColor() {
+		return chatColor;
+	}
 }
