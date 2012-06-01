4974090bfcbd46638d4e672e3b23c46c6f686316
LimeByte <limebyte.endercraft@gmail.com>
2012-06-01 23:04:40 +1200

    APIalize the Arena object and change to suit the new Waypoint object.

--- src/me/limebyte/battlenight/core/Battle/Arena.java
+++ src/me/limebyte/battlenight/core/Battle/Arena.java
@@ -1,55 +1,139 @@
 package me.limebyte.battlenight.core.Battle;
 
+import org.bukkit.Location;
+
 import me.limebyte.battlenight.core.BattleNight;
+import me.limebyte.battlenight.core.Battle.Waypoint.WaypointType;
 
 /**
+ * Represents a Arena.
+ * 
  * @author LimeByte.
  * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-nd/3.0/
  */
 public class Arena {
+    
     private String name;
-    private Waypoints waypoints = new Waypoints(plugin, this);
-    private boolean inUse = false;
+    private String displayName;
+    private Waypoint redSpawn = new Waypoint(WaypointType.REDSPAWN, this);
+    private Waypoint blueSpawn = new Waypoint(WaypointType.BLUESPAWN, this);
+    private boolean enabled = true;
 
     // Get Main Class
     public static BattleNight plugin;
+    public Arena(BattleNight instance) {
+        plugin = instance;
+    }
+    
+    ////////////////////
+    //  Constructors  //
+    ////////////////////
     
     /**
-     * Create a new arena by specifying the instance of
-     * BattleNight and the name for this Arena.
+     * Create a new arena by specifying the name for this Arena.
      */
-    public Arena(BattleNight instance, String name) {
-        plugin = instance;
-        this.name = name;
+    public Arena(String name) {
+        name = this.name;
     }
 
+    ////////////////////
+    //    Getters     //
+    ////////////////////
     
+    /**
+     * Gets the name of this Arena.
+     * @return the current name
+     */
     public String getName() {
         return name;
     }
 
-    public Waypoints getWaypoints() {
-        return waypoints;
+    /**
+     * Gets the "friendly" name of the Arena.
+     * @return the current display name
+     */
+    public String getDisplayName() {
+        return displayName;
     }
 
-    public boolean isInUse() {
-        return inUse;
+    /**
+     * Gets the Arena's Location for the red team to spawn.
+     * @return the current red spawn location
+     */
+    public Location getRedSpawn() {
+        return redSpawn.getLocation();
+    }
+
+    /**
+     * Gets the Arena's Location for the blue team to spawn.
+     * @return the current blue spawn location
+     */
+    public Location getBlueSpawn() {
+        return blueSpawn.getLocation();
     }
     
+
+    ////////////////////
+    //    Setters     //
+    ////////////////////
+    
+    /**
+     * Sets the "friendly" name of the Arena.
+     * @param the new display name
+     */
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    /**
+     * Sets the Arena's Location for the red team to spawn.
+     * @param the new red spawn location
+     */
+    public void setRedSpawn(Location location) {
+        redSpawn.setLocation(location);
+    }
+
+    /**
+     * Sets the Arena's Location for the blue team to spawn.
+     * @param the new blue spawn location
+     */
+    public void setBlueSpawn(Location location) {
+        blueSpawn.setLocation(location);
+    }
+    
+    /**
+     * Sets the Arena's status to enabled.
+     */
+    public void enable() {
+        enabled = true;
+    }
+    
+    /**
+     * Sets the Arena's status to disabled.
+     */
+    public void disable() {
+        enabled = false;
+    }
+
+    ////////////////////
+    //    Checkers    //
+    ////////////////////
+    
+    /**
+     * Checks if the Arena is setup.
+     * @return whether it is setup
+     */
     public boolean isSetup() {
-        for(Waypoints.Waypoint w : Waypoints.Waypoint.values()) {
-            if(!waypoints.isSet(w)) return false;
-        }
-        return true;
+        return (redSpawn.isSet() && blueSpawn.isSet());
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setInUse(boolean inUse) {
-        this.inUse = inUse;
+    
+    /**
+     * Checks if the Arena is enabled.
+     * @return whether it is enabled
+     */
+    public boolean isEnabled() {
+        return enabled;
     }
 
 }
