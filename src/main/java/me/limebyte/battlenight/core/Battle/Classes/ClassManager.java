9c53ee7ec76d12d3c4142c08408a3c869528f181
LimeByte <limebyte@twf.co.nz>
2012-07-30 16:37:49 +1200

    Fixed some errors in class loader.

--- src/main/java/me/limebyte/battlenight/core/Battle/Classes/ClassManager.java
+++ src/main/java/me/limebyte/battlenight/core/Battle/Classes/ClassManager.java
@@ -3,8 +3,12 @@ package me.limebyte.battlenight.core.Battle.Classes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
 import me.limebyte.battlenight.core.BattleNight;
@@ -67,6 +71,7 @@ public class ClassManager {
 			if (item == null) continue;
 			
 			int amount = 1;
+			Map<Enchantment, Integer> enchantments = new HashMap<Enchantment, Integer>();
 			short data = 0;
 			
 			String[] part1 = item.split("x");
@@ -87,23 +92,76 @@ public class ClassManager {
 				
 				try {
 					a = Integer.parseInt(part1[1]);
-				} catch (NumberFormatException e) {}
+				} catch (NumberFormatException ex) {}
 				
 				amount = a;
 			}
 
+			// Do we have any enchantments?
+			if (part2.length == 2) {
+				String[] part4 = part2[1].split("|");
+				
+				for (String s : part4) {
+					String[] splitEnchantment = s.replace("(", "").replace(")", "").split("~");
+					int e = -1;
+					int lvl = 1;
+					Enchantment enc;
+					
+					// Checks
+					if (splitEnchantment.length == 0) continue;
+					
+					
+					// Get the enchantment id
+					try {
+						e = Integer.parseInt(splitEnchantment[0]);
+					} catch (NumberFormatException ex) {
+						continue;
+					}
+					
+					enc = Enchantment.getById(e);
+					
+					// More Checks
+					if (enc == null) continue;
+					
+					// Do we have a level?
+					if (splitEnchantment.length == 2) {
+						try {
+							lvl = Integer.parseInt(splitEnchantment[1]);
+						} catch (NumberFormatException ex) {}
+					}
+					else {
+						lvl = enc.getStartLevel();
+					}
+					
+					// Cap levels
+					if (lvl > enc.getMaxLevel()) lvl = enc.getMaxLevel();
+					
+					// Add it
+					enchantments.put(enc, lvl);
+				}
+			}
+			
 			// Do we have any data?
 			if (part3.length == 2) {
 				short d = 0;
 				
 				try {
 					d = Short.parseShort(part3[1]);
-				} catch (NumberFormatException e) {}
+				} catch (NumberFormatException ex) {}
 				
 				data = d;
 			}
 			
 			ItemStack stack = new ItemStack(id, 1, data);
+			
+			if (!enchantments.isEmpty()) {
+				Iterator<Entry<Enchantment, Integer>> it = enchantments.entrySet().iterator();
+				while (it.hasNext()) {
+					try {
+						stack.addEnchantment(it.next().getKey(), it.next().getValue());
+					} catch (Exception ex) {} 
+				}
+			}
 			
 			if (amount > 1) {
 				items.addAll(Arrays.asList(splitIntoStacks(stack, amount)));
@@ -121,11 +179,32 @@ public class ClassManager {
     	String rawItems = "";
     	
     	for (ItemStack item : items) {
-    		int id = item.getTypeId();
-    		int data = item.getDurability();
-    		int amount = item.getAmount();
     		
-    		rawItems += ", " + id + ":" + data + "x" + amount;
+    		if (item == null) {
+    			rawItems += ", none";
+    		}
+    		else {
+	    		int id = item.getTypeId();
+	    		int data = item.getDurability();
+	    		int amount = item.getAmount();
+	    		Map<Enchantment, Integer> enchantments =  item.getEnchantments();
+	    		
+	    		rawItems += ", " + id;
+	    		if (data > 0)  rawItems += ":" + data;
+	    		if (!item.getEnchantments().isEmpty()) {
+	    			String rawEnchantments = "";
+	    			rawItems += "e(";
+	    			
+	    			Iterator<Entry<Enchantment, Integer>> it = enchantments.entrySet().iterator();
+	    			while(it.hasNext()) {
+	    				rawEnchantments += "|" + it.next().getKey() + "~" + it.next().getValue();
+	    			}
+	    			rawItems += rawEnchantments.substring(1);
+	    			
+	    			rawItems += ")";
+	    		}
+	    		if (amount > 1) rawItems += "x" + amount;
+    		}
     	}
     	
     	return rawItems.substring(2);
@@ -135,19 +214,23 @@ public class ClassManager {
     private ItemStack[] splitIntoStacks(ItemStack item, int amount) {
     	final int maxSize = item.getMaxStackSize();
     	final int fullStacks = (int) Math.floor(amount / maxSize);
+    	final int finalStackAmount = amount % maxSize;
     	 
     	ItemStack fullStack = item.clone();
     	ItemStack finalStack = item.clone();
     	fullStack.setAmount(maxSize);
-    	finalStack.setAmount(amount % maxSize);
+    	finalStack.setAmount(finalStackAmount);
     	
-    	ItemStack[] items = new ItemStack[fullStacks+1];
+    	ItemStack[] items;
+    	
+    	if (finalStackAmount > 0) items = new ItemStack[fullStacks+1];
+    	else items = new ItemStack[fullStacks];
     	
     	for (int i=0; i<fullStacks; i++) {
     		items[i] = fullStack;
     	}
     	
-    	items[items.length - 1] = finalStack;
+    	if (finalStackAmount > 0) items[items.length - 1] = finalStack;
     	
     	return items;
     }
