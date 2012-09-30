5bf676537a92128b5909ad5e37320715af69c4a9
LimeByte <limebyte@twf.co.nz>
2012-09-30 14:17:09 +1300

    Added GameMode option and updated version info to 1.2.8.

--- src/main/java/me/limebyte/battlenight/core/BattleNight.java
+++ src/main/java/me/limebyte/battlenight/core/BattleNight.java
@@ -1186,7 +1186,7 @@ public class BattleNight extends JavaPlugin {
 	    	p.setExhaustion(0);
 	    	p.setLevel(0);
 	    	p.setExp(0);
-	    	p.setGameMode(GameMode.SURVIVAL);
+	    	p.setGameMode(GameMode.getByValue(config.getInt("GameMode", 0)));
 	    	p.setAllowFlight(false);
 	    	p.setFlying(false);
 	    	p.setSleepingIgnored(true);
