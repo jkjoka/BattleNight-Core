bbeee9d9dc9bfa22df9ab8eebb4f2468171b76b6
LimeByte <limebyte@twf.co.nz>
2012-09-30 23:23:42 +1300

    Refresh the players tag on join.

--- src/main/java/me/limebyte/battlenight/core/BattleNight.java
+++ src/main/java/me/limebyte/battlenight/core/BattleNight.java
@@ -1223,6 +1223,7 @@ public class BattleNight extends JavaPlugin {
     	
  	    player.setPlayerListName(pListName.length() < 16 ? pListName : pListName.substring(0, 16));
     	player.setDisplayName(ChatColor.GRAY + "[BN] " + teamColour + name + ChatColor.RESET);
+    	TagAPI.refreshPlayer(player);
 	}
 	
 	private void removePotionEffects(Player p) {
